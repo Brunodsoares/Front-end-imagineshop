44a1adad64de7f16df56244174e77501823bb99e
Bruno <bruno.sdsoares@gmail.com>
2022-07-22 20:01:06 -0300

    commit init

---
+++ components/Header.tsx
@@ -0,0 +1,77 @@
+import { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import styled from "styled-components";
+
+import Logo from "../assets/logo.png";
+import ShoppingCartLogo from "../assets/carrinho.png";
+import { Container } from "../styles/utils";
+
+const Header: NextPage = () => {
+  return (
+    <StyledHeader>
+      <Navbar>
+        <Image
+          src={Logo}
+          width={200}
+          height={100}
+          alt="logo imagine shop em dua cores"
+        />
+        <MenuList>
+          <MenuItem>
+            <Link href="/">Home</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/about">Sobre</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/shopping-cart">
+              <a>
+                <Image
+                  src={ShoppingCartLogo}
+                  width={52}
+                  height={52}
+                  alt="link e icone da pagina para carrinho de compras"
+                />
+              </a>
+            </Link>
+          </MenuItem>
+        </MenuList>
+      </Navbar>
+    </StyledHeader>
+  );
+};
+
+const StyledHeader = styled.header`
+  width: 100vw;
+  margin: 1.87rem 0 3.125rem 0;
+`;
+const Navbar = styled.nav`
+  ${Container};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const MenuList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  gap: 2.5rem;
+  align-items: center;
+`;
+
+const MenuItem = styled.li`
+  font-size: 16px;
+  font-weight: 700;
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+  a:hover {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+export default Header;
---
+++ pages/_app.tsx
@@ -0,0 +1,45 @@
+import type { AppProps } from "next/app";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import Layout from "../components/Layout";
+config.autoAddCss = false;
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+
+  :root{
+    font-size: 16px
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: #fff;
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#f73f01",
+    secondary: "#777",
+    backfooter: "#f4f4f4",
+  },
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
