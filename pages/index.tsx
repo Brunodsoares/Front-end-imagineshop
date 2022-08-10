5b28f8f990d4b2d9f7719b7348c3a20d98941c4b
Bruno <bruno.sdsoares@gmail.com>
2022-08-09 23:40:05 -0300

    feat: creating product component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import type { NextPage } from "next";
+import styled from "styled-components";
+
+import Banner from "../components/Banner";
+import Products from "../components/Products";
+import BannerImage from "../public/images/BANNER01.png";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <h1>Hello Word!</h1>
-    </>
+    <Main>
+      <Banner image={BannerImage} width={1140} heigth={325} />
+      <Products products={[]} />
+    </Main>
   );
 };
+
+const Main = styled.main`
+  min-height: 47vh;
+
+`;
 
 export default Home;
